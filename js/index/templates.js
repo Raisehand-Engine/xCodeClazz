366e298805cca28a208d1432cab2f52a86ee0ba4
Ryanhustler420 <gouravgupta840@gmail.com>
2022-07-08 12:18:31 +0530

    added more logo

--- js/index/templates.js
+++ js/index/templates.js
@@ -19,8 +19,8 @@ function singleCourseDesign(course) {
                 ${course.features.map(e => `<li>${e}</li>`).join('')}
             </ul>
             <div class="flex justify-center items-center">
-                <button class="border px-4 py-2 bg-black text-white" ${!course.hasActive ? 'disabled' : ''} onclick="${course.hasActive ? `openRequestCallback('course', '${course._id}')` : `showSnackbar('Please dont hack me!')`}">Request</button>
-                <a class="border px-4 py-2 bg-logoColor text-white" href="/scope.html?courseId=${course._id}" >Scope</a>
+                <button class="border px-4 py-2 bg-logoColor text-white" ${!course.hasActive ? 'disabled' : ''} onclick="${course.hasActive ? `openRequestCallback('course', '${course._id}')` : `showSnackbar('Please dont hack me!')`}">Request</button>
+                <!--<a class="border px-4 py-2 bg-logoColor text-white" href="/scope.html?courseId=${course._id}" >Scope</a>-->
                 <a class="border px-4 py-2 text-white ${!course.hasActive ? 'disabled' : ''}" href="/editor.html?lang=${course.keywords}" ><img src="/assets/code-editor-logo.svg" alt="/assets/code-editor-logo.svg" class="h-6 w-6"></a>
             </div>
         </div>
