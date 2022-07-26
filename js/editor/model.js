7473e341ddf8c2db30cc251538cde390cad48992
Ryanhustler420 <gouravgupta840@gmail.com>
2022-07-26 12:40:40 +0530

    added error log

--- js/editor/model.js
+++ js/editor/model.js
@@ -3,7 +3,11 @@ var modal = document.getElementById("outputModal");
 function closeModel() { modal.style.display = "none"; }
 function openConsoleOutput(data) {
     modal.style.display = "block";
-    document.getElementById("output-te").innerHTML = data.stdout;
+    if (data.stderr) {
+        document.getElementById("output-te").innerHTML = data.stderr;
+    } else if (data.stdout) {
+        document.getElementById("output-te").innerHTML = data.stdout;
+    }
 }
 
 // When the user clicks anywhere outside of the modal, close it
